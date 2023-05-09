c701c48c127fb495c53fce7d4d5fdf00c815f9da
rafa <rafa@pc95-11.14005298.14.andared.ced.junta-andalucia.es>
2023-05-09 11:04:45 +0200

    Leves retoques a la BD

--- Proyecto/src/main/resources/squema.sql
+++ Proyecto/src/main/resources/squema.sql
@@ -1,17 +1,17 @@
-drop table if exists plataforma;
-drop table if exists producto;
-drop table if exists genero;
+drop table if exists Plataforma;
+drop table if exists Producto;
+drop table if exists Genero;
 drop sequence if exists hibernate_sequence;
 create sequence hibernate_sequence start with 100 increment by 1;
 
 
-create table genero (
+create table Genero (
 	nombre varchar(512), 
     descripcion blob,
 	primary key (nombre)
 );
 
-create table juego (
+create table Juego (
 	id bigint not null auto_increment, 
 	nombre varchar(512), 
 	genero varchar(512),
@@ -21,11 +21,11 @@ create table juego (
 	imagen varchar(512), 
     plataforma bigint,
 	primary key (id),
-    constraint fk_juego_genero foreign key (genero) references genero(nombre),
-    constraint fk_juego_plataforma foreign key (plataforma) references plataforma(id)
+    constraint fk_juego_genero foreign key (genero) references Genero(nombre),
+    constraint fk_juego_plataforma foreign key (plataforma) references Plataforma(id)
 );
 
-create table plataforma (
+create table Plataforma (
 	id bigint not null auto_increment, 
 	nombrePlataforma varchar(20),
     descripcion blob,
